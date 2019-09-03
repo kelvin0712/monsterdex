01d245cb7a774f41bbd0d953f94e9970b0ed6921
Kelvin <duchung97dl@gmail.com>
2019-09-03 23:14:03 +1000

    Updates

---
+++ precache-manifest.f67e2cdeaa7420f76f2004b9334293e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5814f2a3e3d938dbafa1b6415527b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afab04552ae9d61609a6",
+    "url": "/static/css/main.58b887b7.chunk.css"
+  },
+  {
+    "revision": "0544c80799e2a000cefd",
+    "url": "/static/js/2.8f0b785f.chunk.js"
+  },
+  {
+    "revision": "afab04552ae9d61609a6",
+    "url": "/static/js/main.c13633f4.chunk.js"
+  },
+  {
+    "revision": "36fb1394e6792534bc17",
+    "url": "/static/js/runtime~main.e3743821.js"
+  }
+]);
